dd6ad1d8896492ee0cec7d0b8688860ed470e252
joao pedro marangoni <joao200213pedro@gmail.com>
2022-11-05 19:51:18 -0300

    feat: Criando backend de enderecos e adicionando requisições do insomnia

---
+++ backend/address/src/main/java/com/codeforce/hackathon/model/Address.java
@@ -0,0 +1,25 @@
+package com.codeforce.hackathon.model;
+
+import java.math.BigDecimal;
+
+import com.mongodb.client.model.geojson.Point;
+import com.mongodb.client.model.geojson.Position;
+
+import io.quarkus.mongodb.panache.PanacheMongoEntity;
+import io.quarkus.mongodb.panache.common.MongoEntity;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@MongoEntity(collection="address")
+@Getter
+@Setter
+@ToString
+public class Address extends PanacheMongoEntity {
+    public String street;
+    public String city;
+    public String state;
+    public String postalCode;
+    public String country;
+    public Point geoLocale;
+}
---
+++ backend/address/src/main/java/com/codeforce/hackathon/model/UpdateDTO.java
@@ -0,0 +1,20 @@
+package com.codeforce.hackathon.model;
+
+import java.math.BigDecimal;
+
+import lombok.Data;
+import lombok.ToString;
+
+@Data
+@ToString
+public class UpdateDTO {
+    private String id;
+    private String street;
+    private Integer number;
+    private String city;
+    private String state;
+    private String postalCode;
+    private String country;
+    private Double x;
+    private Double y;
+}
