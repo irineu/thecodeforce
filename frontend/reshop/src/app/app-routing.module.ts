d159357b8fc013b6003c91b03ea0bb749ec35277
Irineu Antunes <irineu@irineuantunes.com>
2022-11-05 10:55:51 -0300

    navigation & login sso

--- frontend/reshop/src/app/app-routing.module.ts
+++ frontend/reshop/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {LoginComponent} from "./login/login.component";
+import {HomeComponent} from "./home/home.component";
+import {CheckoutComponent} from "./checkout/checkout.component";
+import {CheckoutCompleteComponent} from "./checkout-complete/checkout-complete.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'checkout', component: CheckoutComponent },
+  { path: 'checkout-complete', component: CheckoutCompleteComponent },
+  { path: '**', component: LoginComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- frontend/reshop/src/app/login/login.component.ts
+++ frontend/reshop/src/app/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {MockService} from "../mock.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private mockService:MockService, private router:Router) { }
 
   ngOnInit(): void {
+  }
+
+  loginWithSSO(platform:string){
+    this.mockService.mockUserFrom(platform);
+    this.router.navigate(['home']);
   }
 
 }
