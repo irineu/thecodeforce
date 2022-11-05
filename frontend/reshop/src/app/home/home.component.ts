2a18db9b33eaac5e3a9b926bdc4d6ccc42f917dc
Irineu Antunes <irineu@irineuantunes.com>
2022-11-05 12:15:20 -0300

    nova tela

--- frontend/reshop/src/app/home/home.component.ts
+++ frontend/reshop/src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {MockService} from "../mock.service";
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private mockService:MockService) { }
+
+  activeUser: any;
 
   ngOnInit(): void {
+    this.activeUser = this.mockService.getActiveUser();
   }
 
 }
--- frontend/reshop/src/app/mock.service.ts
+++ frontend/reshop/src/app/mock.service.ts
@@ -7,7 +7,90 @@ export class MockService {
 
   constructor() { }
 
-  mockUserFrom(platform: string) {
+  products = [
+    //Papel Toalha
+    {
+      name: 'Papel Toalha',
+      brand: 'Kitchen',
+      type: 'Papel Toalha',
+      units: 2,
+      avgUnitDuration: 5
+    },
+    {
+      name: 'Papel Toalha',
+      brand: 'Qualitá',
+      type: 'Papel Toalha',
+      units: 3,
+      avgUnitDuration: 5
+    },
 
+    //Café
+    {
+      name: 'Café Tradicional',
+      brand: 'Três Corações',
+      type: 'Café',
+      units: 1,
+      avgUnitDuration: 15
+    },
+    {
+      name: 'Café Extra Forte',
+      brand: 'Três Corações',
+      type: 'Café',
+      units: 1,
+      avgUnitDuration: 15
+    },
+    {
+      name: 'Café Tradicional',
+      brand: 'Três Pelé',
+      type: 'Café',
+      units: 1,
+      avgUnitDuration: 15
+    },
+
+    //Desinfetante
+    {
+      name: 'Desinfetante Lavanda 1l',
+      brand: 'Pinho',
+      type: 'Desinfetante',
+      units: 1,
+      avgUnitDuration: 5
+    },
+    {
+      name: 'Desinfetante Lavanda 2l',
+      brand: 'Pinho',
+      type: 'Desinfetante',
+      units: 1,
+      avgUnitDuration: 10
+    }
+  ]
+
+  platformMock = {
+    "linkedin" : {
+      name: "Irineu Antunes",
+      address: {
+        addrLine1: "Rua XPTO, 161",
+        addrLine2: "apto 34",
+        city: "São Paulo",
+        state: "SP",
+        zip: "12345-000"
+      },
+      productList: [],
+      periodicity: '30d'
+    }
+  }
+
+  activeUser: any = null;
+
+  mockUserFrom(platform: string) {
+    // @ts-ignore
+    this.activeUser = this.platformMock[platform];
+  }
+
+  getActiveUser() {
+    if(!this.activeUser){
+      this.activeUser = this.platformMock["linkedin"];
+    }
+
+    return this.activeUser;
   }
 }
