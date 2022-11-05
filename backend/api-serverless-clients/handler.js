a1775009ddcfc28948ff1e27a595a2e84b53bbf2
Fabio Molliet <fmolliet@hotmail.com>
2022-11-05 12:16:42 -0300

    feat: Correção do metodo delete de clientes, e melhoria na exibição de erros

--- backend/api-serverless-clients/handler.js
+++ backend/api-serverless-clients/handler.js
@@ -20,37 +20,52 @@ app.use(morgan('tiny'));
  * @param {*} message
  * @returns
  */
- const createErrorResponse = (statusCode, message) => ({
-  statusCode: statusCode || 501,
-  headers: { 'Content-Type': 'application/json' },
-  body: JSON.stringify({
+ const createErrorResponse = (message) => ({
     error: message || 'Ocorreu um erro.',
-  }),
-});
+  });
 
 
 app.get("/", async (req, res, next) => {
-  const result = await clients.find().toArray();
-  return res.status(200).json(result);
+  try {
+    const result = await clients.find().toArray();
+    return res.status(200).json(result);
+  }
+  catch (error) {
+    return res.status(500).json(createErrorResponse(error.message));
+  }
 });
 
 app.post("/", async (req, res, next) => {
-  clients.insertOne({ nome: req.body.nome, cpf: req.body.cpf})
-  return res.status(201).json({
-    message: "Criado com sucesso!",
-  });
+  try {
+    clients.insertOne({ nome: req.body.nome, cpf: req.body.cpf})
+    return res.status(201).json({
+      message: "Criado com sucesso!",
+    });
+  }
+  catch (error) {
+    return res.status(500).json(createErrorResponse(error.message));
+  }
 });
 
 app.patch("/", async (req, res, next) => {
-  clients.findOneAndUpdate({ _id: ObjectId(req.body.id) }, {$set: { nome: req.body.nome, cpf: req.body.cpf}}, { upsert: true } )
-  return res.status(202).json({
-    message: "Dados atualizados com sucesso!"
-  });
+  try {
+    clients.findOneAndUpdate({ _id: ObjectId(req.body.id) }, {$set: { nome: req.body.nome, cpf: req.body.cpf}}, { upsert: true } )
+    return res.status(202).json({
+      message: "Dados atualizados com sucesso!"
+    });
+  }
+  catch (error) {
+    return res.status(500).json(createErrorResponse(error.message));
+  }
 });
 
 app.delete("/", async (req, res, next) => {
-  clients.deleteOne({ cpf: req.body.cpf })
-  return res.status(204);
+  try {
+    await clients.deleteOne({ _id: ObjectId(req.body.id) })
+    return res.status(204).send();
+  } catch (error) {
+    return res.status(500).json(createErrorResponse(error.message));
+  }
 });
 
 app.use( async (req, res, next) => {
